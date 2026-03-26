ebac87866ff41dce6134255e57764e4c1c0a30cb
CrisPSdev <c.pardos.sp@gmail.com>
2026-03-26 12:10:32 -0300

    nuevo

---
+++ src/test/java/cl/duoc/registroClientes/RegistroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.registroClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
